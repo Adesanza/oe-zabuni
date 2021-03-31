62ae77f358e05ba6fcb3e51d8f718b5d1b938663
Adeshina <badmusadeshinna@gmail.com>
2021-03-31 14:17:32 +0100

    minor edit

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import DisplayBillboards from '../../components/billboards-display/billboards-display';
-import BillboardsTable from '../../components/billboards-table/billboard-table';
+// import BillboardsTable from '../../components/billboards-table/billboard-table';
 import DateLastUpdated from '../../components/date-lastUpdated-display/date-lastUpdated';
 import SearchInputField from '../../components/search-input-field/search-input-field';
 import SideBar from '../../components/side-bar/side-bar';
@@ -24,7 +24,7 @@ const Dashboard = () => {
                         <div>
                         <h1>Hello i will be the graph and i am awaiting a full data.. winks</h1>
                         </div>
-                        <BillboardsTable />
+                        <DisplayBillboards />
                     </div>
                 }
                 {/* uncomment the code above and comment the one below to test the dashboard out if the one above cannot be sorted out comment it out and use the one below*/}

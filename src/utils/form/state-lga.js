9bf1cb2b934a4f08a72af9defcb0409cc21cb7bf
Tunde <tundealabi4780@gmail.com>
2021-04-20 12:18:57 +0100

    edit data for billboard state, lga and city

--- src/utils/form/state-lga.js
+++ src/utils/form/state-lga.js
@@ -5,7 +5,40 @@ const getLgaCity = (lga) => cityData[lga];
 
 
 const lgaData = {
-    abia: ['Aba North', 'Aba South', 'Arochukwu', 'Bende', 'Ikwuano', 'Isiala Ngwa North', 'Isiala Ngwa South', 'Isuikwuato', 'Obi Ngwa', 'Ohafia', 'Osisioma', 'Ugwunagbo', 'Ukwa East', 'Ukwa West', 'Umuahia North', 'Umuahia South', 'Umu Nneochi'],
+    abia: [
+        {lga:'Aba North',
+    cities:['Ogbo',
+        'Uratta',
+        'Eziama',
+        'Osusu',
+        'Umuola-Egbule',
+        'Umuola-Okpulor',
+        'Umuokoji'
+    ]}, 
+        {lga:'Aba South', 
+    cities:['Asaeme',
+        'Ihieorji',
+        'Ndiegori',
+        'Nnetu',
+        'Ohabiam',
+        'Umuagbai',
+        'Umumb',
+        'Umuosi',
+        'Abaukwu',
+        'Ariaria',
+        'Asaokpuja',
+        'Amanfuru'
+    ]}, 
+        {lga:'Arochukwu', 
+    cities:['Arochukwu',
+        'Ujari',
+        'Amasu',
+        'Ibom',
+        'Amuze',
+        'Oro',
+        'Obinkita',
+        'Amoba'
+    ]}, 'Bende', 'Ikwuano', 'Isiala Ngwa North', 'Isiala Ngwa South', 'Isuikwuato', 'Obi Ngwa', 'Ohafia', 'Osisioma', 'Ugwunagbo', 'Ukwa East', 'Ukwa West', 'Umuahia North', 'Umuahia South', 'Umu Nneochi'],
     adamawa: ['Demsa', 'Fufure', 'Ganye', 'Gayuk', 'Gombi', 'Grie', 'Hong', 'Jada', 'Larmurde', 'Madagali', 'Maiha', 'Mayo Belwa', 'Michika', 'Mubi North', 'Mubi South', 'Numan', 'Shelleng', 'Song', 'Toungo', 'Yola North', 'Yola South'],
     akwa_ibom: ['Abak', 'Eastern Obolo', 'Eket', 'Esit Eket', 'Essien Udim', 'Etim Ekpo', 'Etinan', 'Ibeno', 'Ibesikpo Asutan', 'Ibiono Ibom', 'Ika', 'Ikono', 'Ikot Abasi', 'Ikot Ekpene', 'Ini', 'Itu', 'Mbo', 'Mkpat Enin', 'Nsit Atai', 'Nsit Ibom', 'Nsit Ubium', 'Obot Akara', 'Okobo', 'Onna', 'Oron', 'Oruk Anam', 'Udung Uko', 'Ukanafun', 'Uruan', 'Urue Offong Oruko', 'Uyo'],
     anambra: ['Aguata', 'Anambra East', 'Anambra West', 'Anaocha', 'Awka North', 'Awka South', 'Ayamelum', 'Dunukofia', 'Ekwusigo', 'Idemili North', 'Idemili South', 'Ihiala', 'Njikoka', 'Nnewi North', 'Nnewi South', 'Ogbaru', 'Onitsha North', 'Onitsha South', 'Orumba North', 'Orumba South', 'Oyi'],

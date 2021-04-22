1d5a0233689410c22a4d91425cdd0c78c4df3711
Adeshina <badmusadeshinna@gmail.com>
2021-04-22 16:02:58 +0100

    implement Sorting toggle button ( Name, Amount, region, type and state) in billboard table

--- src/components/billboards-table/billboard-table.jsx
+++ src/components/billboards-table/billboard-table.jsx
@@ -6,144 +6,208 @@ import { useDispatch } from 'react-redux';
 import { verticalModalContent } from '../../redux/vertical-modal/verticalModalReducer';
 import { editBillboardData } from '../../redux/form/billboardFormReducer';
 // import { billboardFilter } from '../../utils/billboard-table/filter-billboard';
-import { formatBillboardLocation, formatBillboardState, formatBillboardType } from '../../utils/billboard-table/format-text';
+import {
+  formatBillboardLocation,
+  formatBillboardState,
+  formatBillboardType,
+} from '../../utils/billboard-table/format-text';
 import { overheadModalContainer } from '../../redux/overhead-modal/overheadModalReducer';
 import { setConfirmationAction } from '../../redux/confirmation/confirmationPopupReducer';
 import RingLoader from '../loader/loader';
-import { getTableHeaderProps } from './table-header-data';
+import { tableHeaderData } from './table-header-data';
+import { useState } from 'react';
+import { filterBillboardData } from '../../redux/filter-Billboard/filterBillboardReducer';
 
 // import TablePagination from '../pagination/pagination';
-
-const BillboardsTable = ({showCreate, billboardData, filteredBillboardData, isLoadingData}) => {
+const BillboardsTable = ({
+  showCreate,
+  billboardData,
+  filteredBillboardData,
+  isLoadingData,
+}) => {
+  const [tableHead, updateTableHead] = useState(tableHeaderData);
   const dispatch = useDispatch();
-  const handleEdit = (id,moreDeatils = false) => {
-    const toBeEdited = billboardData.find(billboard => billboard._id === id);
+  const handleEdit = (id, moreDeatils = false) => {
+    const toBeEdited = billboardData.find((billboard) => billboard._id === id);
     dispatch(editBillboardData(toBeEdited));
-    if(!moreDeatils) dispatch(verticalModalContent('edit-billboard'))
-  }
-    return (
-        <div className="billboard-table hermis">
-        <Table >
-  <thead className="lightert">
-    <tr>
-      {
-          getTableHeaderProps().getTableHeaderData()
-          .map(({ headName, isSorted, isSortedDesc}) => (
-            <th>
-              <span>{ headName }</span>
-              {
-                isSorted ?
-                  isSortedDesc ?
-                    <span className="sort-btn" ><FaSortDown /></span> 
-                    :
-                    <span className="sort-btn" ><FaSortUp /></span>
-                    :
-                    null
-              }
-            </th>
-          ))
+    if (!moreDeatils) dispatch(verticalModalContent('edit-billboard'));
+  };
+  const handleTableHead = (searchType, searchValue, headerIdx) => {
+    const updateTableHeadMap = tableHead.map((tableHeader, idx) => {
+      if (idx === headerIdx) {
+        return {
+          ...tableHeader,
+          isSortedDesc: !tableHeader.isSortedDesc,
+          isSortedAsc: !tableHeader.isSortedAsc,
+        };
       }
-      {
-        showCreate ?
-        <th><Button type="button" className="create-billboard-btn" onClick={() => {
-          alert("Creating of billboard is currently under construction.")
-          // dispatch(verticalModalContent('create-billboard'))
-        }}>CREATE NEW</Button></th>
-        :
-        null
-      }
-      
-    </tr>
-  </thead>
-  <tbody className="evenad">
-        {
-          isLoadingData ?
-          <tr className="table-loading">
-            <td colSpan={100}>
-             <RingLoader borderColor="#0056b3" />
-            </td>
-           
+      return { ...tableHeader, isSortedDesc: true, isSortedAsc: false };
+    });
+    dispatch(filterBillboardData({ type: searchType, keyword: searchValue }));
+    updateTableHead(updateTableHeadMap);
+  };
+
+  return (
+    <div className="billboard-table hermis">
+      <Table>
+        <thead className="lightert">
+          <tr>
+            {tableHead.map(({ headName, isSorted, isSortedDesc }, idx) => (
+              <th
+                key={idx}
+                className={isSorted ? 'show-cursor' : ''}
+                onClick={() => {
+                  if (isSorted) {
+                    handleTableHead(
+                      'sort-by-header',
+                      `${headName}-${isSortedDesc ? 'desc' : 'asc'}`,
+                      idx
+                    );
+                  }
+                }}
+              >
+                <span>{headName}</span>
+                {isSorted ? (
+                  isSortedDesc ? (
+                    <span className="sort-btn">
+                      <FaSortDown />
+                    </span>
+                  ) : (
+                    <span className="sort-btn">
+                      <FaSortUp />
+                    </span>
+                  )
+                ) : null}
+              </th>
+            ))}
+            {showCreate ? (
+              <th>
+                <Button
+                  type="button"
+                  className="create-billboard-btn"
+                  onClick={() => {
+                    alert(
+                      'Creating of billboard is currently under construction.'
+                    );
+                    // dispatch(verticalModalContent('create-billboard'))
+                  }}
+                >
+                  CREATE NEW
+                </Button>
+              </th>
+            ) : null}
           </tr>
-          :
-            filteredBillboardData.result.map(details => (
-                <tr key={details._id} className="billboard-row table-hover" 
-                    onClick={() => {
-                      handleEdit(details._id,true)
-                      dispatch(verticalModalContent('more-details'))
-                    }}>
-                    <td className="fonter">{details.name}</td>
-                    <td className="locate">{formatBillboardLocation(details.location)}</td>
-                    <td className="fonter">{formatBillboardType(details.type)}</td>
-                    <td className="fonter">
-                        <span>{`${details.height_m}m`}</span>
-                      {
-                        details.class === 'digital' ?
-                        <>
-                        <br />
-                        <span>{`${details.height_px}px`}</span>
-                        </>
-                        :
-                        null
-                      }
-                    </td>
-                    <td className="fonter">
-                        <span>{`${details.width_m}m`}</span>
-                      {
-                        details.class === 'digital' ?
-                        <>
-                        <br />
-                        <span>{`${details.width_px}px`}</span>
-                        </>
-                        :
-                        null
-                      }
-                    </td>
-                    <td className={details.status==='active' ? 'active' : details.status==='inactive' ? 'inactive' : 'vacant'} >{details.status}</td>
-                    <td className="fonter">{details.category}</td>
-                    <td className="fonter">{details.class}</td>
-                    <td className="fonter">{details.face}</td>
-                    <td className="fonter">{details.slot}</td>
-                    <td className="fonter">{details.unit}</td>
-                    <td className="fonter">{details.region}</td>
-                    <td className="fonter">{formatBillboardState(details.state)}</td>
-                    <td className="fonter">{details.lga}</td>
-                    <td className="fonter">{details.city}</td>
-                    <td className="fonter">{details.coordinate}</td>
-                    <td className="fonter billboard-amount"><span></span>{details.amount}<span>Per annum</span></td>
-                    {
-                      showCreate ?
-                      <td className="fonter">
-                      <div className="edit-delete-container">
-                      <div onClick={(e) => {
-                        e.stopPropagation();
-                        // handleEdit(details._id)
-                        alert("Editing of billboard is currently under construction.")
-                      }}>
-                        <RiPencilLine className="edit-icon"/>
+        </thead>
+        <tbody className="evenad">
+          {isLoadingData ? (
+            <tr className="table-loading">
+              <td colSpan={100}>
+                <RingLoader borderColor="#0056b3" />
+              </td>
+            </tr>
+          ) : (
+            filteredBillboardData.result.map((details) => (
+              <tr
+                key={details._id}
+                className="billboard-row table-hover"
+                onClick={() => {
+                  handleEdit(details._id, true);
+                  dispatch(verticalModalContent('more-details'));
+                }}
+              >
+                <td className="fonter">{details.name}</td>
+                <td className="locate">
+                  {formatBillboardLocation(details.location)}
+                </td>
+                <td className="fonter">{formatBillboardType(details.type)}</td>
+                <td className="fonter">
+                  <span>{`${details.height_m}m`}</span>
+                  {details.class === 'digital' ? (
+                    <>
+                      <br />
+                      <span>{`${details.height_px}px`}</span>
+                    </>
+                  ) : null}
+                </td>
+                <td className="fonter">
+                  <span>{`${details.width_m}m`}</span>
+                  {details.class === 'digital' ? (
+                    <>
+                      <br />
+                      <span>{`${details.width_px}px`}</span>
+                    </>
+                  ) : null}
+                </td>
+                <td
+                  className={
+                    details.status === 'active'
+                      ? 'active'
+                      : details.status === 'inactive'
+                      ? 'inactive'
+                      : 'vacant'
+                  }
+                >
+                  {details.status}
+                </td>
+                <td className="fonter">{details.category}</td>
+                <td className="fonter">{details.class}</td>
+                <td className="fonter">{details.face}</td>
+                <td className="fonter">{details.slot}</td>
+                <td className="fonter">{details.unit}</td>
+                <td className="fonter">{details.region}</td>
+                <td className="fonter">
+                  {formatBillboardState(details.state)}
+                </td>
+                <td className="fonter">{details.lga}</td>
+                <td className="fonter">{details.city}</td>
+                <td className="fonter">{details.coordinate}</td>
+                <td className="fonter billboard-amount">
+                  <span></span>
+                  {details.amount}
+                  <span>Per annum</span>
+                </td>
+                {showCreate ? (
+                  <td className="fonter">
+                    <div className="edit-delete-container">
+                      <div
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          // handleEdit(details._id)
+                          alert(
+                            'Editing of billboard is currently under construction.'
+                          );
+                        }}
+                      >
+                        <RiPencilLine className="edit-icon" />
                         <span className="edit-icon-text">Edit</span>
                       </div>
-                      <div onClick={(e) => {
-                        e.stopPropagation();
-                        dispatch(setConfirmationAction({type:'delete-billboard', id: details._id}))
-                        dispatch(overheadModalContainer('confirmation'))
-                        // dispatch(deleteBillboard(details.id));
-                      }}>
-                        <RiDeleteBin6Line className="delete-icon"/>
+                      <div
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          dispatch(
+                            setConfirmationAction({
+                              type: 'delete-billboard',
+                              id: details._id,
+                            })
+                          );
+                          dispatch(overheadModalContainer('confirmation'));
+                          // dispatch(deleteBillboard(details.id));
+                        }}
+                      >
+                        <RiDeleteBin6Line className="delete-icon" />
                         <span className="delete-icon-text">Delete</span>
                       </div>
-                      </div>
-                    </td>
-                      :
-                      null
-                    }
-                </tr>
+                    </div>
+                  </td>
+                ) : null}
+              </tr>
             ))
-        }
-  </tbody>
-</Table>
-{/* <TablePagination currentTablePage={currentPage} pageData={filteredBillboardData.result}  setCurrentPage={setCurrentPage} pageEnd={filteredBillboardData.pageEnd} gotoNextPage={filteredBillboardData.nextPage}/> */}
-</div>
-    )
-}
+          )}
+        </tbody>
+      </Table>
+      {/* <TablePagination currentTablePage={currentPage} pageData={filteredBillboardData.result}  setCurrentPage={setCurrentPage} pageEnd={filteredBillboardData.pageEnd} gotoNextPage={filteredBillboardData.nextPage}/> */}
+    </div>
+  );
+};
 
 export default BillboardsTable;
--- src/utils/billboard-table/filter-billboard.js
+++ src/utils/billboard-table/filter-billboard.js
@@ -1,31 +1,73 @@
-const billboardFilter = (filterData,billboardData,tablePage,tableSize) => {
-    switch (filterData.type) {
-        case 'search-by-name':
-            return paginateBillboard(tablePage,tableSize,billboardData.filter(details => details.name.toLowerCase().includes(filterData.keyword.toLowerCase())));
-        case 'search-by-location':
-            return paginateBillboard(tablePage,tableSize,billboardData.filter(details => details.location.toLowerCase().includes(filterData.keyword.toLowerCase())));
-        case 'category':
-            return paginateBillboard(tablePage, tableSize,billboardData.filter(details => details.category.toLowerCase() === filterData.keyword.toLowerCase()));
-        default:
-            return paginateBillboard(tablePage,tableSize,billboardData);
-    }
-}
+import { split } from 'lodash';
+
+const billboardFilter = (filterData, billboardData, tablePage, tableSize) => {
+  switch (filterData.type) {
+    case 'search-by-name':
+      return paginateBillboard(
+        tablePage,
+        tableSize,
+        billboardData.filter((details) =>
+          details.name.toLowerCase().includes(filterData.keyword.toLowerCase())
+        )
+      );
+    case 'search-by-location':
+      return paginateBillboard(
+        tablePage,
+        tableSize,
+        billboardData.filter((details) =>
+          details.location
+            .toLowerCase()
+            .includes(filterData.keyword.toLowerCase())
+        )
+      );
+    case 'category':
+      return paginateBillboard(
+        tablePage,
+        tableSize,
+        billboardData.filter(
+          (details) =>
+            details.category.toLowerCase() === filterData.keyword.toLowerCase()
+        )
+      );
+    case 'sort-by-header':
+      return paginateBillboard(
+        tablePage,
+        tableSize,
+        billboardData.sort((a, b) => {
+          let splitKeyword = filterData.keyword.split('-');
+          let propA =
+            splitKeyword[0] !== 'amount'
+              ? a[splitKeyword[0]].toLowerCase()
+              : Number(a[splitKeyword[0]].split(' ')[0].replaceAll(/,/g, ''));
+          let propB =
+            splitKeyword[0] !== 'amount'
+              ? b[splitKeyword[0]].toLowerCase()
+              : Number(b[splitKeyword[0]].split(' ')[0].replaceAll(/,/g, ''));
+          if (splitKeyword[1] === 'desc')
+            return propA < propB ? -1 : propA > propB ? 1 : 0;
+          return propA > propB ? -1 : propA < propB ? 1 : 0;
+        })
+      );
+    default:
+      return paginateBillboard(tablePage, tableSize, billboardData);
+  }
+};
 
 const paginateBillboard = (tablePage, tableSize, data) => {
-    // console.log("paginate",data)
-    let result = data.slice((tablePage - 1) * tableSize, tablePage * tableSize);
-    let pageEnd = false;
-    let nextPage = true;
-    if(tablePage === 1 && result.length < tableSize) {
-        nextPage = false;
-        pageEnd = true;
-    }else if(result.length < tableSize) pageEnd  = true;
+  // console.log("paginate",data)
+  let result = data.slice((tablePage - 1) * tableSize, tablePage * tableSize);
+  let pageEnd = false;
+  let nextPage = true;
+  if (tablePage === 1 && result.length < tableSize) {
+    nextPage = false;
+    pageEnd = true;
+  } else if (result.length < tableSize) pageEnd = true;
 
-    return {
-        result,
-        nextPage,
-        pageEnd
-    };
-}
+  return {
+    result,
+    nextPage,
+    pageEnd,
+  };
+};
 
 export { billboardFilter };

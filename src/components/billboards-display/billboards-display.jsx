a677152c8e08e98947e2bbae9e83d122325982f6
Adeshina <badmusadeshinna@gmail.com>
2021-04-29 15:23:10 +0100

    edit billboard table, search input and formreducer

--- src/components/billboards-display/billboards-display.jsx
+++ src/components/billboards-display/billboards-display.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useBillboardData } from '../../hooks/billboard-data-hook';
-import { billboardFilter } from "../../utils/billboard-table/filter-billboard";
-import BillboardCategory from "../billboard-category/billboard-category";
-import BillboardsTable from "../billboards-table/billboard-table";
-import TablePagination from "../pagination/pagination";
-import "./billboards-display.css";
+import { billboardFilter } from '../../utils/billboard-table/filter-billboard';
+import BillboardCategory from '../billboard-category/billboard-category';
+import BillboardsTable from '../billboards-table/billboard-table';
+import TablePagination from '../pagination/pagination';
+import './billboards-display.css';
 
 const DisplayBillboards = ({ showCreate, showCategoryNav }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -13,30 +13,39 @@ const DisplayBillboards = ({ showCreate, showCategoryNav }) => {
   const { billboardData, isLoading, isError } = useBillboardData();
   // console.log("bill",billboardData);
   // const billboardData = useSelector(state => state.billboardData);
-  const filterKey = useSelector(state => state.filterBillboard);
-  const filteredBillboardData = billboardFilter(filterKey,billboardData,currentPage,tableSize);
+  const filterKey = useSelector((state) => state.filterBillboard);
+  const filteredBillboardData = billboardFilter(
+    filterKey,
+    billboardData,
+    currentPage,
+    tableSize
+  );
   useEffect(() => {
-    setCurrentPage(1)
-  }, [filterKey.keyword])
-  
+    setCurrentPage(1);
+  }, [filterKey.keyword]);
+
   return (
     <div className="display-billboards">
-      {
-        showCategoryNav ? 
-          <BillboardCategory />
-          :
-          null
-      }
-      {
-        !isError ? 
+      {showCategoryNav ? <BillboardCategory /> : null}
+      {!isError ? (
         <>
-        <BillboardsTable showCreate={showCreate} setCurrentPage={setCurrentPage} filterKey={filterKey} billboardData={billboardData} filteredBillboardData={filteredBillboardData} isLoadingData={isLoading} />
-      <TablePagination currentTablePage={currentPage} pageData={filteredBillboardData.result}  setCurrentPage={setCurrentPage} pageEnd={filteredBillboardData.pageEnd} gotoNextPage={filteredBillboardData.nextPage} />
-      </>
-      :
-      null
-      }
-      
+          <BillboardsTable
+            showCreate={showCreate}
+            setCurrentPage={setCurrentPage}
+            filterKey={filterKey}
+            billboardData={billboardData}
+            filteredBillboardData={filteredBillboardData}
+            isLoadingData={isLoading}
+          />
+          <TablePagination
+            currentTablePage={currentPage}
+            pageData={filteredBillboardData.result}
+            setCurrentPage={setCurrentPage}
+            pageEnd={filteredBillboardData.pageEnd}
+            gotoNextPage={filteredBillboardData.nextPage}
+          />
+        </>
+      ) : null}
     </div>
   );
 };
--- src/redux/form/billboardFormReducer.js
+++ src/redux/form/billboardFormReducer.js
@@ -1,71 +1,88 @@
+import { createSlice } from '@reduxjs/toolkit';
 import {
-    createSlice
-} from "@reduxjs/toolkit";
-import { getRegionState, getStateLga, getLgaCity } from "../../utils/form/compute-state-data";
+  getRegionState,
+  getStateLga,
+  getLgaCity,
+} from '../../utils/form/compute-state-data';
 
 const init = {
-    isEditing: false,
-    formData: {
-        name: '',
-        location: '',
-        type: '',
-        height_m: '',
-        width_m: '',
-        height_px: '',
-        width_px: '',
-        status: '',
-        category: '',
-        class: '',
-        region: '',
-        state: '',
-        coordinate: '',
-        lga: '',
-        city: '',
-        amount: '',
-        face: '',
-        slot: '',
-        unit: ''
-    },
-    currentRegion: '',
-    currentState: '',
-    stateData: [],
-    lgaData: [],
-    cityData: []
-    
-}
+  isEditing: false,
+  formData: {
+    name: '',
+    location: '',
+    type: '',
+    height_m: '',
+    width_m: '',
+    height_px: '',
+    width_px: '',
+    status: '',
+    category: '',
+    class: '',
+    region: '',
+    state: '',
+    coordinate: '',
+    lga: '',
+    city: '',
+    amount: '',
+    face: '',
+    slot: '',
+    unit: '',
+  },
+  currentRegion: '',
+  currentState: '',
+  stateData: [],
+  lgaData: [],
+  cityData: [],
+};
 
 const billboardFormReducer = createSlice({
-    name: 'billboardForm',
-    initialState: init,
-    reducers: {
-        editBillboardData: (state, action) => {
-            state.isEditing = true;
-            state.formData = {
-                ...action.payload
-            }
-            // state.lgaData = [...getStateLga(action.payload.state)]
-        },
-        showStateData: (state, action) => {
-            state.currentRegion = action.payload;
-            state.stateData = [...getRegionState(action.payload)];
-        },
-        showLgaData: (state, action) => {
-            state.currentState = action.payload;
-            state.lgaData = [...getStateLga(action.payload,state.currentRegion)]
-        },
-        showCityData: (state, action) => {
-            state.cityData = [...getLgaCity({region: state.currentRegion, state: state.currentState, lga: action.payload})];
-        },
-        resetBillboardFormData: (state, action) => init
-    }
-})
+  name: 'billboardForm',
+  initialState: init,
+  reducers: {
+    editBillboardData: (state, action) => {
+      state.isEditing = true;
+      state.formData = {
+        ...action.payload,
+      };
+      state.stateData = [...getRegionState(action.payload.region)];
+      state.lgaData = [
+        ...getStateLga(action.payload.state, action.payload.region),
+      ];
+      state.cityData = [
+        ...getLgaCity({
+          region: action.payload.region,
+          state: action.payload.state,
+          lga: action.payload.lga,
+        }),
+      ];
+    },
+    showStateData: (state, action) => {
+      state.currentRegion = action.payload;
+      state.stateData = [...getRegionState(action.payload)];
+    },
+    showLgaData: (state, action) => {
+      state.currentState = action.payload;
+      state.lgaData = [...getStateLga(action.payload, state.currentRegion)];
+    },
+    showCityData: (state, action) => {
+      state.cityData = [
+        ...getLgaCity({
+          region: state.currentRegion,
+          state: state.currentState,
+          lga: action.payload,
+        }),
+      ];
+    },
+    resetBillboardFormData: (state, action) => init,
+  },
+});
 
 export const {
-    editBillboardData,
-    showStateData,
-    showLgaData,
-    showCityData,
-    resetBillboardFormData
+  editBillboardData,
+  showStateData,
+  showLgaData,
+  showCityData,
+  resetBillboardFormData,
 } = billboardFormReducer.actions;
 
 export default billboardFormReducer.reducer;
